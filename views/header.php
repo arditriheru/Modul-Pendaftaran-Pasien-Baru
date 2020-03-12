224b521cf6c5dfa7c344ddf6e3899865cdca9581
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-03-12 11:46:51 +0700

    versi1

---
+++ views/header.php
@@ -0,0 +1,28 @@
+<?php error_reporting(0); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Dashboard | Pendaftaran Online</title>
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <!-- Add custom CSS here -->
+    <link href="../css/sb-admin.css" rel="stylesheet">
+    <link rel="stylesheet" href="../font-awesome/css/font-awesome.min.css">
+    <style>
+	.bluetext {
+	color: #008cba;
+	}
+    .navbar-rachmi{
+        background-color:#e67e22;
+        border-color:#d35400
+    }
+    .navbar-brand{
+        color:#ffffff;
+    }
+
+</style>
+  </head>
